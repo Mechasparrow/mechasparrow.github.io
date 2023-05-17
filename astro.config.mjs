8673b23ed68384a07c745b5fe9cc58e3b7579d5b
Michael Navazhylau <mikipux7@gmail.com>
2023-05-17 00:00:41 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mechasparrow.com',
+  site: 'https://www.mechasparrow.com',
   integrations: [tailwind()]
 });
